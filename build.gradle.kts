f0473822aad3b066e5240e1f9c181a3a34a2c064
Clément Fournier <clement.fournier76@gmail.com>
2018-12-16 18:14:14 +0100

    Setup publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.dokka.gradle.DokkaTask
 plugins {
     kotlin("jvm") version "1.2.41"
     id("org.jetbrains.dokka") version "0.9.17"
+    `maven-publish`
 
 }
 
@@ -17,7 +18,7 @@ repositories {
 
 dependencies {
 
-    compile(kotlin("stdlib-jdk7"))
+    implementation(kotlin("stdlib-jdk7"))
     compile(kotlin("test"))
 
     // test use case
@@ -26,18 +27,67 @@ dependencies {
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.1.11")
 }
 
+tasks.getByName<Test>("test") {
+    useJUnitPlatform { }
+}
 
 
-tasks {
+val dokka by tasks.getting(DokkaTask::class) {
+    outputFormat = "html"
+    outputDirectory = "$buildDir/javadoc"
 
-    getByName<DokkaTask>("dokka") {
-        outputFormat = "html"
-        outputDirectory = "$buildDir/javadoc"
+    includes += "dokka.md"
+}
 
-        includes += "dokka.md"
-    }
+val dokkaJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles Kotlin docs with Dokka"
+    classifier = "javadoc"
+    from(dokka)
+}
 
-    getByName<Test>("test") {
-        useJUnitPlatform { }
+val sourcesJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles sources JAR"
+    classifier = "sources"
+    from(java.sourceSets["main"].allSource)
+}
+
+
+with(project.extensions) {
+
+    val publishing = getByType(PublishingExtension::class.java)
+
+    with(publishing) {
+
+        publications {
+
+            create<MavenPublication>("default") {
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+
+                from(components["java"])
+                artifact(dokkaJar)
+                artifact(sourcesJar)
+
+                pom {
+
+                    name.set("Kotlin Tree Matchers")
+                    packaging = "jar"
+                    description.set("A testing DSL to specify the structure of a tree in a concise and readable way.")
+                    url.set("https://github.com/oowekyala/kt-tree-matchers")
+
+                }
+            }
+        }
+
+
+        repositories {
+            //            mavenCentral()
+            maven {
+                url = uri("$buildDir/repository")
+            }
+        }
     }
 }
